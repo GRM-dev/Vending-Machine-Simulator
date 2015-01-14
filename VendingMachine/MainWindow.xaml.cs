4e70a412b22a0bfbbeed077d04d99ae993943f4b
Admaster <mizyn24@gmail.com>
2015-01-14 21:47:01 +0100

    Added Metro Style

--- VendingMachine/MainWindow.xaml.cs
+++ VendingMachine/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,7 @@ namespace VendingMachine
     /// Automat. Zawiera cala kontrole automate wraz widokiem MVC
     /// Klasa Main
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private VMachine vMachine;
 
