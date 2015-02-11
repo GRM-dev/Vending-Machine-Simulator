d29677dbc5f758e0488dc81ff9556eceecc526c1
Admaster <mizyn24@gmail.com>
2015-02-11 12:15:08 +0100

    start/stop simulation buttons and speed slider

--- VendingMachine/XAML/SimulationFlyout.xaml.cs
+++ VendingMachine/XAML/SimulationFlyout.xaml.cs
@@ -20,12 +20,15 @@ namespace VendingMachine.XAML
     /// </summary>
     public partial class SimulationFlyout : UserControl
     {
+        private Boolean _running;
+
         /// <summary>
         /// Main Constructor for Flyout
         /// </summary>
         public SimulationFlyout()
         {
             InitializeComponent();
+            SimulationRunning = false;
         }
 
         /// <summary>
@@ -34,6 +37,28 @@ namespace VendingMachine.XAML
         public void Update()
         {
            
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            SimulationRunning = true;
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            SimulationRunning = false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Boolean SimulationRunning
+        {
+            get { return _running; }
+            set { _running = value;
+            StartButton.IsEnabled = !_running;
+            StopButton.IsEnabled = _running;
+            }
         }
     }
 }
